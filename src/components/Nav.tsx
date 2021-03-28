a20b7623db74bafe8af3f41ebd5b1a10f61bb5fd
Hyyy_1 <447542807@qq.com>
2021-03-28 14:59:34 +0800

    创建 Ico n组件

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,9 +3,7 @@ import styled from 'styled-components';
 import {
   Link
 } from "react-router-dom";
-require('../icons/money.svg')
-require('../icons/tag.svg');
-require('../icons/chart.svg');
+import Icon from './icon';
 
 
 
@@ -38,21 +36,15 @@ const Nav = ()=>{
     <NavWrapper>
         <ul>
             <li>
-              <svg className="icon" >
-                <use xlinkHref="#tag"/>
-              </svg> 
+              <Icon name="tag"/>
               <Link to="/tags">标签</Link>
             </li>
             <li>
-            <svg  className="icon">
-                <use xlinkHref="#money"/>
-              </svg>
+            <Icon name="money"/>
               <Link to="/money">记账</Link>
             </li>
             <li>
-            <svg className="icon">
-                <use xlinkHref="#chart"/>
-              </svg>
+            <Icon name="chart"/>
               <Link to="/statistics">统计</Link>
             </li>
           </ul>
