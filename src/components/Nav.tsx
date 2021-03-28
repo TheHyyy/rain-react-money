526efd6e71a56ad951ed37298dfc7102e3107195
Hyyy_1 <447542807@qq.com>
2021-03-28 14:27:47 +0800

    搞定底部icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 import {
   Link
 } from "react-router-dom";
-import x from '../icons/money.svg'
+require('../icons/money.svg')
+require('../icons/tag.svg');
+require('../icons/chart.svg');
 
-console.log(x)
+
 
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
@@ -16,6 +18,16 @@ const NavWrapper = styled.nav`
         width:33.3333%;
         text-align:center;
         padding:16px;
+        display:flex;
+        flex-direction:column;
+        justify-content:center;
+        align-items:center;
+        padding:4px 0;
+        .icon{
+          width:24px;
+          height:24px;
+        }
+      
     }
   }
 
@@ -26,13 +38,21 @@ const Nav = ()=>{
     <NavWrapper>
         <ul>
             <li>
-              <img src={x} alt="" />
+              <svg className="icon" >
+                <use xlinkHref="#tag"/>
+              </svg> 
               <Link to="/tags">标签</Link>
             </li>
             <li>
+            <svg  className="icon">
+                <use xlinkHref="#money"/>
+              </svg>
               <Link to="/money">记账</Link>
             </li>
             <li>
+            <svg className="icon">
+                <use xlinkHref="#chart"/>
+              </svg>
               <Link to="/statistics">统计</Link>
             </li>
           </ul>
